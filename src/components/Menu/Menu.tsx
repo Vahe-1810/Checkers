550ef9791d710562ba8ad27600be7c3acd62503f
Vahe-1810 <vahevardanyan2000@gmail.com>
2023-11-29 15:42:39 +0400

    init checkers app

---
+++ src/components/Menu/Menu.tsx
@@ -0,0 +1,12 @@
+import "./menu.css";
+
+const Menu = () => {
+  return (
+    <div className="menu">
+      <div className="menu-title"></div>
+      <div className="menu-content"></div>
+    </div>
+  );
+};
+
+export default Menu;
