5f5b651dd9538a12c109f87e6215ae6aba757e09
AndranikDeveloper <andranikagabekyan2005@gmail.com>
2023-11-29 17:46:32 +0400

    added App features

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
+import { useState } from "react";
 import "./App.css";
 import Menu from "./components/Menu/Menu";
+import Game from "./components/Game";
 
 function App() {
-  return (
-    <div className="app">
-      <Menu />
-    </div>
-  );
+  const [isGame, setIsGame] = useState(false);
+  return <div className="app">{isGame ? <Game /> : <Menu setIsGame={setIsGame} />}</div>;
 }
 
 export default App;
